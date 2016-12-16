b7652a6c6aa3b7273fc2330b75f862a61c363dcc
naravitchan <naravit_out@hotmail.com>
2016-12-16 08:08:03 +0700

    add station(location in firebase)

--- app/src/main/java/com/egco428/mutram/DataList.java
+++ app/src/main/java/com/egco428/mutram/DataList.java
@@ -9,13 +9,15 @@ public class DataList {
     private String detail;
     private String lat;
     private String longitude;
+    private String station;
 
-    public DataList( String message ,String pic, String detail, String lat, String longitude ){
+    public DataList( String message ,String pic, String detail, String lat, String longitude,String station ){
         this.pic =pic;
         this.message = message;
         this.detail = detail;
         this.lat = lat;
         this.longitude= longitude;
+        this.station= station;
     }
 
     public String getMessage(){ return message; }
@@ -23,6 +25,7 @@ public class DataList {
     public String getDetail(){ return detail; }
     public String getLat() {return lat;}
     public String getLongitude() {return longitude;}
+    public String getStation() {return station;}
 
 
 }
